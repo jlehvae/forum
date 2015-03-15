6bc94b3cc7887fd6c4b211e82429881b1871a6ad
jykke <jyri.lehva@saunalahti.fi>
2015-03-15 14:06:43 +0200

    User has the same avatar as in Github, show post count per discussion on front page and added placeholder for likes per post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
   def create_oauth
     login = env["omniauth.auth"]["extra"]["raw_info"]["login"]
+    avatar = env["omniauth.auth"]["extra"]["raw_info"]["avatar_url"]
 
     if login
       user = User.find_by username: login
 
       if user
         session[:user_id] = user.id
+        if user.avatar != avatar
+          user.avatar = avatar
+          user.save()
+        end
         redirect_to :root, notice: "Welcome back!"
       else
-        newUser = User.new(username: login)
+        newUser = User.new(username: login, avatar: avatar)
         newUser.save()
         session[:user_id] = newUser.id
         redirect_to :root, notice: "Thank you for registering!"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314211435) do
+ActiveRecord::Schema.define(version: 20150315111048) do
 
   create_table "discussions", force: :cascade do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150314211435) do
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
 end
